fa4dc3c7f9edaccfdef3aa2e6388a83da39546d4
Sadovsky33 <kacpersadik@gmail.com>
2025-03-11 10:03:32 +0100

    wstepny kod

--- egzamin_konsolowka/Program.cs
+++ egzamin_konsolowka/Program.cs
@@ -1,4 +1,29 @@
 ﻿class Program
 {
-    static void Mani()
+    static void Main(string[] args)
+    {
+       
+        Console.WriteLine("podaj liczbe a");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("podaj liczbe b ");
+        int b = int.Parse(Console.ReadLine());
+
+       if (a == b)
+        {
+            Console.WriteLine(a);
+
+        }
+
+        if (a > b)
+        {
+            a /= a - b;
+            Console.WriteLine(a);
+        }
+        else
+        {
+            b /= b - a;
+            Console.WriteLine(b);
+        }
+        
+    }
 }
